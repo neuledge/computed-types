63ab5ffafea69cb043a54032e35e1829b2bf7269
Moshe Simantov <ms@development.co.il>
2020-03-16 23:36:31 +0200

    test numbers

---
+++ src/validators/number.test.ts
@@ -0,0 +1,39 @@
+import 'mocha';
+import { assert } from 'chai';
+
+import { Float, Integer } from './number';
+
+describe('number', () => {
+  it('Float', () => {
+    assert.equal(Float(123), 123);
+    assert.equal(Float(12.3), 12.3);
+    assert.equal(Float(-12.3), -12.3);
+    assert.equal(Float('12.3'), 12.3);
+    assert.equal(Float('-12.3'), -12.3);
+    assert.equal(Float(null), 0);
+    assert.equal(Float(false), 0);
+    assert.equal(Float(true), 1);
+
+    assert.throws(() => Float('hello'), TypeError);
+    assert.throws(() => Float('2 3'), TypeError);
+    assert.throws(() => Float(undefined), TypeError);
+    assert.throws(() => Float(NaN), TypeError);
+  });
+
+  it('Integer', () => {
+    assert.equal(Integer(123), 123);
+    assert.equal(Integer('12'), 12);
+    assert.equal(Integer('-12'), -12);
+    assert.equal(Integer(null), 0);
+    assert.equal(Integer(false), 0);
+    assert.equal(Integer(true), 1);
+
+    assert.throws(() => Integer('hello'), TypeError);
+    assert.throws(() => Integer('2 3'), TypeError);
+    assert.throws(() => Integer(undefined), TypeError);
+    assert.throws(() => Integer(NaN), TypeError);
+    assert.throws(() => Integer(1.23), TypeError);
+    assert.throws(() => Integer(-1.23), TypeError);
+    assert.throws(() => Integer('12.3'), TypeError);
+  });
+});
