783a55473a5d0b218144c99fbf5134204fa5948e
Moshe Simantov <ms@development.co.il>
2020-05-07 22:46:46 +0300

    fix typescript test

--- src/typescript.test.ts
+++ src/typescript.test.ts
@@ -2,12 +2,15 @@ import Validator from './Validator';
 
 type AssertEqual<T, R> = [R] extends [T] ? ([T] extends [R] ? 'ok' : T) : T;
 
-// type tests
 /* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-explicit-any */
 
-const plainNumber = Validator.proxy((input: number): number => input);
-const num = plainNumber(5);
-const num5 = plainNumber.equals(5)(2);
+describe('TypeScript', () => {
+  it('interfaces', () => {
+    const plainNumber = Validator.proxy((input: number): number => input);
+    const num = plainNumber(5);
+    const num5 = plainNumber.equals(5)(5 as 2);
 
-const test001: AssertEqual<typeof num, number> = 'ok';
-const test002: AssertEqual<typeof num5, 5> = 'ok';
+    const test001: AssertEqual<typeof num, number> = 'ok';
+    const test002: AssertEqual<typeof num5, 5> = 'ok';
+  });
+});
