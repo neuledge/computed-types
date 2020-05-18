42f53e0d7fdbed8e20123ece5adfc2641192efd0
Moshe Simantov <ms@development.co.il>
2020-05-18 13:07:12 +0300

    fix Schema.enum type file

--- src/Schema.ts
+++ src/Schema.ts
@@ -12,114 +12,277 @@ import FunctionType, { FunctionParameters } from './schema/FunctionType';
 import { Enum } from './schema/utils';
 import { enumValue } from './schema/validations';
 
-export default function Schema<S>(
+interface SchemaType {
+  <S>(schema: S, error?: ErrorLike<SchemaParameters<S>>): ValidatorProxy<
+    Validator<SchemaValidatorFunction<S>>
+  >;
+
+  either<A>(
+    ...candidates: [A]
+  ): ValidatorProxy<
+    Validator<FunctionType<SchemaReturnType<A>, SchemaParameters<A>>>
+  >;
+  either<A, B>(
+    ...candidates: [A, B]
+  ): ValidatorProxy<
+    Validator<
+      FunctionType<
+        SchemaReturnType<A> | SchemaReturnType<B>,
+        MergeSchemaParameters<SchemaParameters<A | B>>
+      >
+    >
+  >;
+  either<A, B, C>(
+    ...candidates: [A, B, C]
+  ): ValidatorProxy<
+    Validator<
+      FunctionType<
+        SchemaReturnType<A> | SchemaReturnType<B> | SchemaReturnType<C>,
+        MergeSchemaParameters<SchemaParameters<A | B | C>>
+      >
+    >
+  >;
+  either<A, B, C, D>(
+    ...candidates: [A, B, C, D]
+  ): ValidatorProxy<
+    Validator<
+      FunctionType<
+        | SchemaReturnType<A>
+        | SchemaReturnType<B>
+        | SchemaReturnType<C>
+        | SchemaReturnType<D>,
+        MergeSchemaParameters<SchemaParameters<A | B | C | D>>
+      >
+    >
+  >;
+  either<A, B, C, D, E>(
+    ...candidates: [A, B, C, D, E]
+  ): ValidatorProxy<
+    Validator<
+      FunctionType<
+        | SchemaReturnType<A>
+        | SchemaReturnType<B>
+        | SchemaReturnType<C>
+        | SchemaReturnType<D>
+        | SchemaReturnType<E>,
+        MergeSchemaParameters<SchemaParameters<A | B | C | D | E>>
+      >
+    >
+  >;
+  either<A, B, C, D, E, F>(
+    ...candidates: [A, B, C, D, E, F]
+  ): ValidatorProxy<
+    Validator<
+      FunctionType<
+        | SchemaReturnType<A>
+        | SchemaReturnType<B>
+        | SchemaReturnType<C>
+        | SchemaReturnType<D>
+        | SchemaReturnType<E>
+        | SchemaReturnType<F>,
+        MergeSchemaParameters<SchemaParameters<A | B | C | D | E | F>>
+      >
+    >
+  >;
+  either<A, B, C, D, E, F, G>(
+    ...candidates: [A, B, C, D, E, F, G]
+  ): ValidatorProxy<
+    Validator<
+      FunctionType<
+        | SchemaReturnType<A>
+        | SchemaReturnType<B>
+        | SchemaReturnType<C>
+        | SchemaReturnType<D>
+        | SchemaReturnType<E>
+        | SchemaReturnType<F>
+        | SchemaReturnType<G>,
+        MergeSchemaParameters<SchemaParameters<A | B | C | D | E | F | G>>
+      >
+    >
+  >;
+  either<A, B, C, D, E, F, G, H>(
+    ...candidates: [A, B, C, D, E, F, G, H]
+  ): ValidatorProxy<
+    Validator<
+      FunctionType<
+        | SchemaReturnType<A>
+        | SchemaReturnType<B>
+        | SchemaReturnType<C>
+        | SchemaReturnType<D>
+        | SchemaReturnType<E>
+        | SchemaReturnType<F>
+        | SchemaReturnType<G>
+        | SchemaReturnType<H>,
+        MergeSchemaParameters<SchemaParameters<A | B | C | D | E | F | G | H>>
+      >
+    >
+  >;
+  either<A, S>(
+    ...candidates: [A, ...S[]]
+  ): ValidatorProxy<
+    Validator<
+      FunctionType<
+        SchemaReturnType<A> | SchemaReturnType<S>,
+        MergeSchemaParameters<SchemaParameters<A | S>>
+      >
+    >
+  >;
+
+  merge<A>(
+    ...args: [A]
+  ): ValidatorProxy<
+    Validator<FunctionType<SchemaReturnType<A>, SchemaParameters<A>>>
+  >;
+  merge<A, B>(
+    ...args: [A, B]
+  ): ValidatorProxy<
+    Validator<
+      FunctionType<
+        SchemaReturnType<A> & SchemaReturnType<B>,
+        MergeSchemaParameters<SchemaParameters<A> & SchemaParameters<B>>
+      >
+    >
+  >;
+  merge<A, B, C>(
+    ...args: [A, B, C]
+  ): ValidatorProxy<
+    Validator<
+      FunctionType<
+        SchemaReturnType<A> & SchemaReturnType<B> & SchemaReturnType<C>,
+        MergeSchemaParameters<
+          SchemaParameters<A> & SchemaParameters<B> & SchemaParameters<C>
+        >
+      >
+    >
+  >;
+  merge<A, B, C, D>(
+    ...args: [A, B, C, D]
+  ): ValidatorProxy<
+    Validator<
+      FunctionType<
+        SchemaReturnType<A> &
+          SchemaReturnType<B> &
+          SchemaReturnType<C> &
+          SchemaReturnType<D>,
+        MergeSchemaParameters<
+          SchemaParameters<A> &
+            SchemaParameters<B> &
+            SchemaParameters<C> &
+            SchemaParameters<D>
+        >
+      >
+    >
+  >;
+  merge<A, B, C, D, E>(
+    ...args: [A, B, C, D, E]
+  ): ValidatorProxy<
+    Validator<
+      FunctionType<
+        SchemaReturnType<A> &
+          SchemaReturnType<B> &
+          SchemaReturnType<C> &
+          SchemaReturnType<D> &
+          SchemaReturnType<E>,
+        MergeSchemaParameters<
+          SchemaParameters<A> &
+            SchemaParameters<B> &
+            SchemaParameters<C> &
+            SchemaParameters<D> &
+            SchemaParameters<E>
+        >
+      >
+    >
+  >;
+  merge<A, B, C, D, E, F>(
+    ...args: [A, B, C, D, E, F]
+  ): ValidatorProxy<
+    Validator<
+      FunctionType<
+        SchemaReturnType<A> &
+          SchemaReturnType<B> &
+          SchemaReturnType<C> &
+          SchemaReturnType<D> &
+          SchemaReturnType<E> &
+          SchemaReturnType<F>,
+        MergeSchemaParameters<
+          SchemaParameters<A> &
+            SchemaParameters<B> &
+            SchemaParameters<C> &
+            SchemaParameters<D> &
+            SchemaParameters<E> &
+            SchemaParameters<F>
+        >
+      >
+    >
+  >;
+  // merge<A, B, C, D, E, F, G>(
+  //   ...args: [A, B, C, D, E, F, G]
+  // ): ValidatorProxy<
+  //   Validator<
+  //     FunctionType<
+  //       SchemaReturnType<A> &
+  //         SchemaReturnType<B> &
+  //         SchemaReturnType<C> &
+  //         SchemaReturnType<D> &
+  //         SchemaReturnType<E> &
+  //         SchemaReturnType<F> &
+  //         SchemaReturnType<G>,
+  //       MergeSchemaParameters<
+  //         SchemaParameters<A> &
+  //           SchemaParameters<B> &
+  //           SchemaParameters<C> &
+  //           SchemaParameters<D> &
+  //           SchemaParameters<E> &
+  //           SchemaParameters<F> &
+  //           SchemaParameters<G>
+  //       >
+  //     >
+  //   >
+  // >;
+  // merge<A, B, C, D, E, F, G, H>(
+  //   ...args: [A, B, C, D, E, F, G, H]
+  // ): ValidatorProxy<
+  //   Validator<
+  //     FunctionType<
+  //       SchemaReturnType<A> &
+  //         SchemaReturnType<B> &
+  //         SchemaReturnType<C> &
+  //         SchemaReturnType<D> &
+  //         SchemaReturnType<E> &
+  //         SchemaReturnType<F> &
+  //         SchemaReturnType<G> &
+  //         SchemaReturnType<H>,
+  //       MergeSchemaParameters<
+  //         SchemaParameters<A> &
+  //           SchemaParameters<B> &
+  //           SchemaParameters<C> &
+  //           SchemaParameters<D> &
+  //           SchemaParameters<E> &
+  //           SchemaParameters<F> &
+  //           SchemaParameters<G> &
+  //           SchemaParameters<H>
+  //       >
+  //     >
+  //   >
+  // >;
+  merge(
+    ...args: [unknown, ...unknown[]]
+  ): ValidatorProxy<Validator<FunctionType>>;
+
+  enum<E extends Enum<E>, P extends FunctionParameters = [E[keyof E]]>(
+    value: E,
+    error?: ErrorLike<P>,
+  ): ValidatorProxy<Validator<FunctionType<E[keyof E], P>>>;
+}
+
+function schema<S>(
   schema: S,
   error?: ErrorLike<SchemaParameters<S>>,
 ): ValidatorProxy<Validator<SchemaValidatorFunction<S>>> {
   return new Validator(compiler(schema, error)).proxy();
 }
 
-function either<A>(
-  ...candidates: [A]
-): ValidatorProxy<
-  Validator<FunctionType<SchemaReturnType<A>, SchemaParameters<A>>>
->;
-function either<A, B>(
-  ...candidates: [A, B]
-): ValidatorProxy<
-  Validator<
-    FunctionType<
-      SchemaReturnType<A> | SchemaReturnType<B>,
-      MergeSchemaParameters<SchemaParameters<A | B>>
-    >
-  >
->;
-function either<A, B, C>(
-  ...candidates: [A, B, C]
-): ValidatorProxy<
-  Validator<
-    FunctionType<
-      SchemaReturnType<A> | SchemaReturnType<B> | SchemaReturnType<C>,
-      MergeSchemaParameters<SchemaParameters<A | B | C>>
-    >
-  >
->;
-function either<A, B, C, D>(
-  ...candidates: [A, B, C, D]
-): ValidatorProxy<
-  Validator<
-    FunctionType<
-      | SchemaReturnType<A>
-      | SchemaReturnType<B>
-      | SchemaReturnType<C>
-      | SchemaReturnType<D>,
-      MergeSchemaParameters<SchemaParameters<A | B | C | D>>
-    >
-  >
->;
-function either<A, B, C, D, E>(
-  ...candidates: [A, B, C, D, E]
-): ValidatorProxy<
-  Validator<
-    FunctionType<
-      | SchemaReturnType<A>
-      | SchemaReturnType<B>
-      | SchemaReturnType<C>
-      | SchemaReturnType<D>
-      | SchemaReturnType<E>,
-      MergeSchemaParameters<SchemaParameters<A | B | C | D | E>>
-    >
-  >
->;
-function either<A, B, C, D, E, F>(
-  ...candidates: [A, B, C, D, E, F]
-): ValidatorProxy<
-  Validator<
-    FunctionType<
-      | SchemaReturnType<A>
-      | SchemaReturnType<B>
-      | SchemaReturnType<C>
-      | SchemaReturnType<D>
-      | SchemaReturnType<E>
-      | SchemaReturnType<F>,
-      MergeSchemaParameters<SchemaParameters<A | B | C | D | E | F>>
-    >
-  >
->;
-function either<A, B, C, D, E, F, G>(
-  ...candidates: [A, B, C, D, E, F, G]
-): ValidatorProxy<
-  Validator<
-    FunctionType<
-      | SchemaReturnType<A>
-      | SchemaReturnType<B>
-      | SchemaReturnType<C>
-      | SchemaReturnType<D>
-      | SchemaReturnType<E>
-      | SchemaReturnType<F>
-      | SchemaReturnType<G>,
-      MergeSchemaParameters<SchemaParameters<A | B | C | D | E | F | G>>
-    >
-  >
->;
-function either<A, B, C, D, E, F, G, H>(
-  ...candidates: [A, B, C, D, E, F, G, H]
-): ValidatorProxy<
-  Validator<
-    FunctionType<
-      | SchemaReturnType<A>
-      | SchemaReturnType<B>
-      | SchemaReturnType<C>
-      | SchemaReturnType<D>
-      | SchemaReturnType<E>
-      | SchemaReturnType<F>
-      | SchemaReturnType<G>
-      | SchemaReturnType<H>,
-      MergeSchemaParameters<SchemaParameters<A | B | C | D | E | F | G | H>>
-    >
-  >
->;
-function either<A, S>(
+schema.either = function <A, S>(
   ...candidates: [A, ...S[]]
 ): ValidatorProxy<
   Validator<
@@ -139,155 +302,15 @@ function either<A, S>(
       >
     >
   >;
-}
-Schema.either = either;
+};
 
-function merge<A>(
-  ...args: [A]
-): ValidatorProxy<
-  Validator<FunctionType<SchemaReturnType<A>, SchemaParameters<A>>>
->;
-function merge<A, B>(
-  ...args: [A, B]
-): ValidatorProxy<
-  Validator<
-    FunctionType<
-      SchemaReturnType<A> & SchemaReturnType<B>,
-      MergeSchemaParameters<SchemaParameters<A> & SchemaParameters<B>>
-    >
-  >
->;
-function merge<A, B, C>(
-  ...args: [A, B, C]
-): ValidatorProxy<
-  Validator<
-    FunctionType<
-      SchemaReturnType<A> & SchemaReturnType<B> & SchemaReturnType<C>,
-      MergeSchemaParameters<
-        SchemaParameters<A> & SchemaParameters<B> & SchemaParameters<C>
-      >
-    >
-  >
->;
-function merge<A, B, C, D>(
-  ...args: [A, B, C, D]
-): ValidatorProxy<
-  Validator<
-    FunctionType<
-      SchemaReturnType<A> &
-        SchemaReturnType<B> &
-        SchemaReturnType<C> &
-        SchemaReturnType<D>,
-      MergeSchemaParameters<
-        SchemaParameters<A> &
-          SchemaParameters<B> &
-          SchemaParameters<C> &
-          SchemaParameters<D>
-      >
-    >
-  >
->;
-function merge<A, B, C, D, E>(
-  ...args: [A, B, C, D, E]
-): ValidatorProxy<
-  Validator<
-    FunctionType<
-      SchemaReturnType<A> &
-        SchemaReturnType<B> &
-        SchemaReturnType<C> &
-        SchemaReturnType<D> &
-        SchemaReturnType<E>,
-      MergeSchemaParameters<
-        SchemaParameters<A> &
-          SchemaParameters<B> &
-          SchemaParameters<C> &
-          SchemaParameters<D> &
-          SchemaParameters<E>
-      >
-    >
-  >
->;
-function merge<A, B, C, D, E, F>(
-  ...args: [A, B, C, D, E, F]
-): ValidatorProxy<
-  Validator<
-    FunctionType<
-      SchemaReturnType<A> &
-        SchemaReturnType<B> &
-        SchemaReturnType<C> &
-        SchemaReturnType<D> &
-        SchemaReturnType<E> &
-        SchemaReturnType<F>,
-      MergeSchemaParameters<
-        SchemaParameters<A> &
-          SchemaParameters<B> &
-          SchemaParameters<C> &
-          SchemaParameters<D> &
-          SchemaParameters<E> &
-          SchemaParameters<F>
-      >
-    >
-  >
->;
-// function merge<A, B, C, D, E, F, G>(
-//   ...args: [A, B, C, D, E, F, G]
-// ): ValidatorProxy<
-//   Validator<
-//     FunctionType<
-//       SchemaReturnType<A> &
-//         SchemaReturnType<B> &
-//         SchemaReturnType<C> &
-//         SchemaReturnType<D> &
-//         SchemaReturnType<E> &
-//         SchemaReturnType<F> &
-//         SchemaReturnType<G>,
-//       MergeSchemaParameters<
-//         SchemaParameters<A> &
-//           SchemaParameters<B> &
-//           SchemaParameters<C> &
-//           SchemaParameters<D> &
-//           SchemaParameters<E> &
-//           SchemaParameters<F> &
-//           SchemaParameters<G>
-//       >
-//     >
-//   >
-// >;
-// function merge<A, B, C, D, E, F, G, H>(
-//   ...args: [A, B, C, D, E, F, G, H]
-// ): ValidatorProxy<
-//   Validator<
-//     FunctionType<
-//       SchemaReturnType<A> &
-//         SchemaReturnType<B> &
-//         SchemaReturnType<C> &
-//         SchemaReturnType<D> &
-//         SchemaReturnType<E> &
-//         SchemaReturnType<F> &
-//         SchemaReturnType<G> &
-//         SchemaReturnType<H>,
-//       MergeSchemaParameters<
-//         SchemaParameters<A> &
-//           SchemaParameters<B> &
-//           SchemaParameters<C> &
-//           SchemaParameters<D> &
-//           SchemaParameters<E> &
-//           SchemaParameters<F> &
-//           SchemaParameters<G> &
-//           SchemaParameters<H>
-//       >
-//     >
-//   >
-// >;
-function merge(
+schema.merge = function (
   ...args: [unknown, ...unknown[]]
 ): ValidatorProxy<Validator<FunctionType>> {
   return new Validator(mergeSchemas(...(args as [unknown]))).proxy();
-}
+};
 
-Schema.merge = merge;
-
-Schema.enum = function <
+schema.enum = function <
   E extends Enum<E>,
   P extends FunctionParameters = [E[keyof E]]
 >(
@@ -296,3 +319,7 @@ Schema.enum = function <
 ): ValidatorProxy<Validator<FunctionType<E[keyof E], P>>> {
   return new Validator(enumValue(value, error)).proxy();
 };
+
+const Schema: SchemaType = schema;
+
+export default Schema;
